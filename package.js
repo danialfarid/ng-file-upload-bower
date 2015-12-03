30ec6dac1d31f9a482d3d4a768d6313d7911e212
Danial Farid <dan@Danial.local>
2015-12-03 11:17:54 -0500

    10.1.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "danialf:ng-file-upload",
-  "version": "10.1.6",
+  "version": "10.1.7",
   summary: "Lightweight Angular directive to upload files with optional FileAPI shim for cross browser support",
   git: "https://github.com/danialfarid/ng-file-upload.git"
 });
